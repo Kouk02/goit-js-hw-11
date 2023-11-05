c0ac3f93066cdec8da3e622cbe17128439a84737
Kouk02 <126501162+Kouk02@users.noreply.github.com>
2023-11-05 15:12:46 +0000

    Deploying to gh-pages from @ Kouk02/goit-js-hw-11@e2fbecd742486ebcde9dcd958c00e8229bdfc531 🚀

--- index.2afcf1bf.js
+++
@@ -1,2 +0,0 @@
-var e={};const t=new(0,{}.PixabayAPI);document.querySelector(".search-form").addEventListener("submit",(async r=>{r.preventDefault();const a=new FormData(r.target).get("searchQuery");if(!a)return;t.query=a,t.page=1;const n=await t.fetchPhotos();(0,e.renderGallery)(n)}));
-//# sourceMappingURL=index.2afcf1bf.js.map
